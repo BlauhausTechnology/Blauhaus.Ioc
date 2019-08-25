331fea78a17fdebde20147f804c91bd7f9a812b0
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2019-08-25 16:05:22 +0200

    DotNetCoreIocService

---
+++ Blauhaus.Ioc/Blauhaus.Ioc.DotNetCoreIocService/DotNetCoreIocService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Blauhaus.Ioc.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blauhaus.Ioc.DotNetCoreIocService
+{
+    public class DotNetCoreIocService : BaseIocService
+    {
+        private IServiceCollection _serviceCollection;
+        private IServiceProvider _serviceProvider;
+
+        public DotNetCoreIocService(IServiceCollection serviceCollection)
+        {
+            _serviceCollection = serviceCollection;
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        protected override void RegisterTypeWithContainer<T>(IocLifetime lifeTime)
+        {
+            if (lifeTime == IocLifetime.Singleton)
+            {
+                _serviceCollection.AddSingleton<T>();
+                _serviceProvider = _serviceCollection.BuildServiceProvider();
+            }
+            else
+            {
+                _serviceCollection.AddTransient<T>();
+                _serviceProvider = _serviceCollection.BuildServiceProvider();
+            }
+        }
+
+        protected override void RegisterImplementationWithContainer<TInterface, TImplementation>(IocLifetime lifeTime)
+        {
+            if (lifeTime == IocLifetime.Singleton)
+            {
+                _serviceCollection.AddSingleton<TInterface, TImplementation>();
+                _serviceProvider = _serviceCollection.BuildServiceProvider();
+            }
+            else
+            {
+                _serviceCollection.AddTransient<TInterface, TImplementation>();
+                _serviceProvider = _serviceCollection.BuildServiceProvider();
+            }
+        }
+
+        protected override T ResolveFromContainer<T>()
+        {
+            return _serviceProvider.GetService<T>();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _serviceProvider = null;
+            _serviceCollection?.Clear();
+            _serviceCollection = null;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Blauhaus.Ioc
+
+[![Build Status](https://dev.azure.com/blauhaus/Blauhaus.Ioc/_apis/build/status/BlauhausTechnology.Blauhaus.Ioc%20(1)?branchName=release/*)](https://dev.azure.com/blauhaus/Blauhaus.Ioc/_build/latest?definitionId=3&branchName=release/*)
