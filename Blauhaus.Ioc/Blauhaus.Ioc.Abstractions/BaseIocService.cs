3335a36fdc6d49b26300587351aa192ea824978f
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2019-08-25 13:03:35 +0200

    BaseIocService

---
+++ Blauhaus.Ioc/Blauhaus.Ioc.Abstractions/BaseIocService.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace Blauhaus.Ioc.Abstractions
+{
+    public abstract class BaseIocService : IIocService
+    {
+         public void RegisterType<T>(IocLifetime lifeTime = IocLifetime.Transient)
+        {
+            try
+            {
+                RegisterTypeWithContainer<T>(lifeTime);
+            }
+            catch (Exception e)
+            {
+                throw new IocContainerException($"Failed to register {typeof(T).Name} with the Ioc container", e);
+            }
+        }
+
+
+        public void RegisterImplementation<T, TImplementation>(IocLifetime lifeTime = IocLifetime.Transient) where TImplementation : T
+        {
+ 
+            try
+            {
+                RegisterImplementationWithContainer<T, TImplementation>(lifeTime);
+            }
+            catch (Exception e)
+            {
+                throw new IocContainerException($"Failed to register {typeof(TImplementation).Name} as implementation of {typeof(T).Name} with the Ioc container", e);
+            }
+        }
+        
+       
+        public T Resolve<T>()
+        {
+            try
+            {
+                return ResolveFromContainer<T>();
+            }
+            catch (Exception e)
+            {
+                throw new IocContainerException($"Failed to resolve {typeof(T).Name} from the Ioc container", e);
+            }
+        }
+        
+
+        public T ResolveAndInitialize<T, TParam>(TParam param) where T : IInitializable<TParam>
+        {
+            T instance;
+            try
+            {
+                instance = ResolveFromContainer<T>();
+            }
+            catch (Exception e)
+            {
+                throw new IocContainerException($"Failed to resolve {typeof(T).Name} from the Ioc container", e);
+            }
+
+            try
+            {
+                instance.Initialize(param);
+                return instance;
+            }
+            catch (Exception e)
+            {
+                throw new IocContainerException($"Failed to initialize {typeof(T).Name} using parameter {param}", e);
+            }
+
+        }
+
+        public T ResolveAndInitializeById<T>(string param) where T : IInitializable<string>
+        {
+            T instance;
+            try
+            {
+                instance = ResolveFromContainer<T>();
+            }
+            catch (Exception e)
+            {
+                throw new IocContainerException($"Failed to resolve {typeof(T).Name} from the Ioc container", e);
+            }
+
+            try
+            {
+                instance.Initialize(param);
+                return instance;
+            }
+            catch (Exception e)
+            {
+                throw new IocContainerException($"Failed to initialize {typeof(T).Name} using parameter {param}", e);
+            }
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        
+        protected abstract void RegisterTypeWithContainer<T>(IocLifetime lifeTime);
+        protected abstract void RegisterImplementationWithContainer<T, TImplementation>(IocLifetime lifeTime) where TImplementation : T;
+        protected abstract T ResolveFromContainer<T>();
+        protected abstract void Dispose(bool disposing);
+    }
+}
--- Blauhaus.Ioc/Blauhaus.Ioc.DryIocService/DryIocService.cs
+++ Blauhaus.Ioc/Blauhaus.Ioc.DryIocService/DryIocService.cs
@@ -5,7 +5,7 @@ using IContainer = DryIoc.IContainer;
 
 namespace Blauhaus.Ioc.DryIocService
 {
-    public class DryIocService : IIocService
+    public class DryIocService : BaseIocService
     {
         private readonly IContainer _dryIocContainer;
 
@@ -14,92 +14,23 @@ namespace Blauhaus.Ioc.DryIocService
             _dryIocContainer = dryIocContainer; 
         }
         
-        public void RegisterType<T>(IocLifetime lifeTime = IocLifetime.Transient)
+
+        protected override void RegisterTypeWithContainer<T>(IocLifetime lifeTime)
         {
-            try
-            {
-                _dryIocContainer.Register<T>(lifeTime == IocLifetime.Transient ? Reuse.Transient : Reuse.Singleton);
-            }
-            catch (Exception e)
-            {
-                throw new IocContainerException($"Failed to register {typeof(T).Name} with the Ioc container", e);
-            }
+            _dryIocContainer.Register<T>(lifeTime == IocLifetime.Transient ? Reuse.Transient : Reuse.Singleton);
         }
 
-        public void RegisterImplementation<T, TImplementation>(IocLifetime lifeTime = IocLifetime.Transient) where TImplementation : T
+        protected override void RegisterImplementationWithContainer<T, TImplementation>(IocLifetime lifeTime)
         {
- 
-            try
-            {           
-                _dryIocContainer.Register<T, TImplementation>(lifeTime == IocLifetime.Transient ? Reuse.Transient : Reuse.Singleton);
-            }
-            catch (Exception e)
-            {
-                throw new IocContainerException($"Failed to register {typeof(TImplementation).Name} as implementation of {typeof(T).Name} with the Ioc container", e);
-            }
-        }
-        
-       
-        public T Resolve<T>()
-        {
-            try
-            {           
-                return _dryIocContainer.Resolve<T>();
-            }
-            catch (Exception e)
-            {
-                throw new IocContainerException($"Failed to resolve {typeof(T).Name} from the Ioc container", e);
-            }
+            _dryIocContainer.Register<T, TImplementation>(lifeTime == IocLifetime.Transient ? Reuse.Transient : Reuse.Singleton);
         }
 
-        public T ResolveAndInitialize<T, TParam>(TParam param) where T : IInitializable<TParam>
+        protected override T ResolveFromContainer<T>()
         {
-            T instance;
-            try
-            {
-                instance = _dryIocContainer.Resolve<T>();
-            }
-            catch (Exception e)
-            {
-                throw new IocContainerException($"Failed to resolve {typeof(T).Name} from the Ioc container", e);
-            }
-
-            try
-            {
-                instance.Initialize(param);
-                return instance;
-            }
-            catch (Exception e)
-            {
-                throw new IocContainerException($"Failed to initialize {typeof(T).Name} using parameter {param}", e);
-            }
-
+            return _dryIocContainer.Resolve<T>();
         }
 
-        public T ResolveAndInitializeById<T>(string param) where T : IInitializable<string>
-        {
-            T instance;
-            try
-            {
-                instance = _dryIocContainer.Resolve<T>();
-            }
-            catch (Exception e)
-            {
-                throw new IocContainerException($"Failed to resolve {typeof(T).Name} from the Ioc container", e);
-            }
-
-            try
-            {
-                instance.Initialize(param);
-                return instance;
-            }
-            catch (Exception e)
-            {
-                throw new IocContainerException($"Failed to initialize {typeof(T).Name} using parameter {param}", e);
-            }
-        }
-
-        public void Dispose()
+        protected override void Dispose(bool disposing)
         {
             _dryIocContainer?.Dispose();
         }
