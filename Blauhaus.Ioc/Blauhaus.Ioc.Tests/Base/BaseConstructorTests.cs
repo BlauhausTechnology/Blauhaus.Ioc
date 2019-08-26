4af30bbd6b18fa32abb01fcc7d6be6ad490a3a15
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2019-08-26 10:39:34 +0200

    no message

---
+++ Blauhaus.Ioc/Blauhaus.Ioc.Tests/Base/BaseConstructorTests.cs
@@ -0,0 +1,20 @@
+﻿using Blauhaus.Ioc.Abstractions;
+using Blauhaus.Ioc.IntegrationTests.TestObjects;
+using DryIoc;
+using NUnit.Framework;
+
+namespace Blauhaus.Ioc.IntegrationTests.Base
+{
+    public abstract class BaseConstructorTests : BaseIocServiceTest
+    {
+        [Test]
+        public void SHOULD_register_self()
+        {
+            
+            //Act and Assert
+            var iocService = Sut.Resolve<IIocService>();
+            Assert.That(iocService, Is.EqualTo(Sut));
+        }
+
+    }
+}
