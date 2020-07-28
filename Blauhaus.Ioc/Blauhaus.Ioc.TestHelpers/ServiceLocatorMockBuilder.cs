7362b2d731b66ad4b5b7b1eb9db7933b571958ae
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-07-28 16:54:45 +0200

    no message

--- Blauhaus.Ioc/Blauhaus.Ioc.TestHelpers/ServiceLocatorMockBuilder.cs
+++ Blauhaus.Ioc/Blauhaus.Ioc.TestHelpers/ServiceLocatorMockBuilder.cs
@@ -9,28 +9,34 @@ namespace Blauhaus.Ioc.TestHelpers
     {
         public ServiceLocatorMockBuilder Where_Resolve_returns<T>(T value) where T : class
         {
-            Mock.Setup(x => x.Resolve<T>()).Returns(value);
+            Mock.Setup(x => x.Resolve<T>())
+                .Returns(value);
             return this;
         }
         public ServiceLocatorMockBuilder Where_Resolve_throws<T>(string exceptionMessage) where T : class
         {
-            Mock.Setup(x => x.Resolve<T>()).Throws(new Exception(exceptionMessage));
+            Mock.Setup(x => x.Resolve<T>())
+                .Throws(new Exception(exceptionMessage));
             return this;
         }
 
+
         public ServiceLocatorMockBuilder Where_ResolveAs_returns<T>(T value) where T : class
         {
-            Mock.Setup(x => x.ResolveAs<T>(It.IsAny<Type>())).Returns(value);
+            Mock.Setup(x => x.ResolveAs<T>(It.IsAny<Type>()))
+                .Returns(value);
             return this;
         }
         public ServiceLocatorMockBuilder Where_ResolveAs_returns<T>(T value, Type type) where T : class
         {
-            Mock.Setup(x => x.ResolveAs<T>(type)).Returns(value);
+            Mock.Setup(x => x.ResolveAs<T>(type))
+                .Returns(value);
             return this;
         }
         public ServiceLocatorMockBuilder Where_ResolveAs_throws<T>(string exceptionMessage) where T : class
         {
-            Mock.Setup(x => x.ResolveAs<T>(It.IsAny<Type>())).Throws(new Exception(exceptionMessage));
+            Mock.Setup(x => x.ResolveAs<T>(It.IsAny<Type>()))
+                .Throws(new Exception(exceptionMessage));
             return this;
         }
     }
